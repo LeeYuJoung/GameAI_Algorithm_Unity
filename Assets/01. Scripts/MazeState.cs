3fc731a68821d54de4c9d04ad759863bda3d2a91
LeeYuJoung <nania000822@gmail.com>
2024-09-24 15:35:29 +0900

    Initial commit

---
+++ Assets/01. Scripts/MazeState.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Chaper03. 숫자 모으기 미로 구현
+public class MazeState : MonoBehaviour
+{
+    public struct Coord
+    {
+        int _x;
+        int _y;
+    }
+
+    const int H = 3; // 미로의 높이
+    const int W = 4; // 미로의 너비
+    const int END_TURN = 4; // 게임 종료의 턴
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
