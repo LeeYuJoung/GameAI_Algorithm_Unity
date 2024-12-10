9dfa495814ab75bf0e596fece2756bb81fa0e78c
LeeYuJoung <nania000822@gmail.com>
2024-12-10 22:24:54 +0900

    Chapter01

---
+++ Assets/02.Scripts/Chapter01.Action/Steering.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 지능형 움직임을 생성할 뿐만 아니라 지능형 동작을 변경하고 추가하는 모듈식 시스템
+// 에이전트의 이동 및 회전을 저장하기 위한 사용자 정의 데이터
+public class Steering
+{
+    public float angular;
+    public Vector3 linear;
+
+    public Steering()
+    {
+        angular = 0.0f;
+        linear = new Vector3();
+    }
+}
